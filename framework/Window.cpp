e7640181d367bf79ed0552abe301ffd39e0a1074
CarsonFoster <carsonwfoster@gmail.com>
2021-09-14 15:14:36 -0500

    Removed system DPI awareness, since I noticed that the working version still has the errors about RenderTargetView not being compatible with the DepthStencilView Seems like something to look into later, but not the source of the bug

--- framework/Window.cpp
+++ framework/Window.cpp
@@ -115,7 +115,7 @@ Window::Window(Window::WindowInitializationStruct wis)
 	m_clientWidth{ wis.clientWidth }, m_clientHeight{ wis.clientHeight }, kbd{}, mouse{} {
 	
 	// SetProcessDpiAwareness(PROCESS_DPI_AWARENESS::PROCESS_SYSTEM_DPI_AWARE);
-	SetProcessDPIAware();
+	// SetProcessDPIAware();
 
 	HWND hWnd = CreateWindowExW(wis.extendedStyle, wis.className, wis.windowName, wis.windowStyle, wis.x, wis.y,
 		wis.windowWidth, wis.windowHeight, wis.hParent, wis.hMenu, wis.hInstance, this);
